b17a05bcf773dae09147301b4c10a8a23dffd9c3
Jeffrey Zhao <jeffz@live.com>
2012-02-19 01:34:23 +0800

    Load jscex script

---
+++ libs/jscex.js
@@ -0,0 +1,19 @@
+var Jscex = require("jscex");
+require("jscex-jit").init(Jscex);
+require("jscex-async").init(Jscex);
+require("jscex-async-powerpack").init(Jscex);
+
+var Jscexify = Jscex.Async.Jscexify;
+
+var mongoose = require("mongoose");
+
+var mp = mongoose.Model.prototype;
+mp.saveAsync = Jscexify.fromStandard(mp.save);
+mp.removeAsync = Jscexify.fromStandard(mp.remove);
+
+var m = mongoose.Model;
+m.findByIdAsync = Jscexify.fromStandard(m.findById);
+m.findOneAsync = Jscexify.fromStandard(m.findOne);
+m.findAsync = Jscexify.fromStandard(m.find);
+
+exports.Jscex = Jscex;
